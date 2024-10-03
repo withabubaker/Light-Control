91d6f29048852ab1f1a67c2c3e2d5834139c9084
Mohammed <withabubaker@hotmail.com>
2024-10-03 18:23:32 -0400

    config mqtt

--- ControlKasaPlug.py
+++ ControlKasaPlug.py
@@ -1,36 +1,48 @@
 import asyncio
 from kasa import SmartPlug
 from datetime import datetime, time
+import paho.mqtt.client as mqtt
 
 
+mqtt_broker = "192.168.2.13"
+mqtt_topic = "balcony/plug/status"
 
-async def plug_control(plug):
+def publish_status(client, status):
+    client.publish(mqtt_topic, status)
+
+async def plug_control(plug, mqtt_client):
     while True:
         now = datetime.now().time() # get the current time
         turn_on_time =  time(19, 0) # 7pm
         breaking_time = time(23, 59)
         mid_night_time = time (0, 0)
         turn_off_time = time(7, 30) # 7am
-        print(now)
 
         if now >= turn_on_time and now <= breaking_time :
             await plug.turn_on()
             print( plug.alias + " is on")
+            publish_status(mqtt_client, "on")
+
 
         elif now >= mid_night_time and now < turn_off_time:
             await plug.turn_on()
             print( plug.alias + " is on")
+            publish_status(mqtt_client, "on")
+
 
         elif now >= turn_off_time and now < turn_on_time:
             await plug.turn_off()
             print(plug.alias + " is off")
+            publish_status(mqtt_client, "off")
 
         await asyncio.sleep(600) # check evey 10 mins
 
 async def main():
     plug = SmartPlug("192.168.2.28") # plug IP address
     await plug.update() #connect to the plug
-    await plug_control(plug)
+    client = mqtt.Client()
+    client.connect(mqtt_broker)
+    await plug_control(plug, client)
 
 
 
