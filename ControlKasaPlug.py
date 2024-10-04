8ffc32c9017d171282aa1aecda18d7793dbea685
Mohammed <withabubaker@hotmail.com>
2024-10-04 14:22:40 -0400

    fix the issue with mqtt

--- ControlKasaPlug.py
+++ ControlKasaPlug.py
@@ -10,8 +10,10 @@ mqtt_topic = "balcony/plug/status"
 def publish_status(client, status):
     client.publish(mqtt_topic, status)
 
-async def plug_control(plug, mqtt_client):
+async def plug_control(plug):
     while True:
+        client = mqtt.Client()
+        client.connect(mqtt_broker)
         now = datetime.now().time() # get the current time
         turn_on_time =  time(19, 0) # 7:00 pm
         breaking_time = time(23, 59)
@@ -21,28 +23,28 @@ async def plug_control(plug, mqtt_client):
         if now >= turn_on_time and now <= breaking_time :
             await plug.turn_on()  # turn on the light
             #print( plug.alias + " is on")
-            publish_status(mqtt_client, "on") # send mqtt message to node-red
+            publish_status(client, "on") # send mqtt message to node-red
 
 
         elif now >= mid_night_time and now < turn_off_time:
             await plug.turn_on() # turn on the light
             #print( plug.alias + " is on")
-            publish_status(mqtt_client, "on") # send mqtt message to node-red
+            publish_status(client, "on") # send mqtt message to node-red
 
 
         elif now >= turn_off_time and now < turn_on_time:
             await plug.turn_off() # turn off the light
             #print(plug.alias + " is off")
-            publish_status(mqtt_client, "off") # send mqtt message to node-red
+            publish_status(client, "off") # send mqtt message to node-red
 
         await asyncio.sleep(600) # check evey 10 mins
 
 async def main():
     plug = SmartPlug("192.168.2.28") # plug IP address
     await plug.update() #connect to the plug
-    client = mqtt.Client()
-    client.connect(mqtt_broker)
-    await plug_control(plug, client)
+    #client = mqtt.Client()
+    #client.connect(mqtt_broker)
+    await plug_control(plug)
 
 
 
